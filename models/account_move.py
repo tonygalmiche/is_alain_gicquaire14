f9881c4713cf0fb752ca23954f6fbdb35123a336
Tony Galmiche <tony.galmiche@infosaone.com>
2020-11-03 18:03:02 +0100

    bug

--- models/account_move.py
+++ models/account_move.py
@@ -12,23 +12,24 @@ class AccountMoveLine(models.Model):
     @api.model_create_multi
     def create(self, vals):
         res = super(AccountMoveLine, self).create(vals)
-        if res.product_id.is_generer_licence:
-            num_licence = "999999"
-            date_achat  = datetime.date.today().strftime('%d%m%Y')
-            nb_jours    = "365"
-            nb_valid    = "007"
-            x = num_licence+date_achat+nb_jours+nb_valid
-            cle=''
-            sens=1
-            debut=0
-            while debut<10:
-                if sens==1:
-                    y=x[debut:debut+2]
-                else:
-                    y=x[-debut-2:]
-                    y=y[0:2]
-                    debut+=2
-                cle+=y
-                sens=-sens
-            res.is_cle_licence = cle
+        for obj in res:
+            if obj.product_id.is_generer_licence:
+                num_licence = "999999"
+                date_achat  = datetime.date.today().strftime('%d%m%Y')
+                nb_jours    = "365"
+                nb_valid    = "007"
+                x = num_licence+date_achat+nb_jours+nb_valid
+                cle=''
+                sens=1
+                debut=0
+                while debut<10:
+                    if sens==1:
+                        y=x[debut:debut+2]
+                    else:
+                        y=x[-debut-2:]
+                        y=y[0:2]
+                        debut+=2
+                    cle+=y
+                    sens=-sens
+                obj.is_cle_licence = cle
 
